3f53e9f89c7f5cec4871800b249c4c2aa035c86d
ChisatoKasebayashi <chisato.rukia@gmail.com>
2017-03-28 14:19:04 +0900

    add init file, output fusion pose(role pitch and yaw)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 
 int main(void)
 {
+	int sampleCount = 0;
+	int sampleRate = 0;
+	uint64_t rateTimer;
+	uint64_t displayTimer;
+	uint64_t now;
 	RTIMUSettings *settings = new RTIMUSettings("RTIMULib");
 	RTIMU *imu = RTIMU::createIMU(settings);
 
@@ -12,6 +17,38 @@ int main(void)
 		exit(1);
 	}
 	imu->IMUInit();
+
+	imu->setSlerpPower(0.02);
+	imu->setGyroEnable(true);
+	imu->setAccelEnable(true);
+	imu->setCompassEnable(false);
+
+	rateTimer = displayTimer = RTMath::currentUSecsSinceEpoch();
+
+	while(1)
+	{
+		usleep(imu->IMUGetPollInterval() * 1000);
+		while(imu->IMURead())
+		{
+			RTIMU_DATA imuData = imu->getIMUData();
+			sampleCount++;
+			now = RTMath::currentUSecsSinceEpoch();
+
+			if((now - displayTimer) > 1000000)
+			{
+				printf("Sample rate %d: %s\r", sampleRate, RTMath::displayDegrees("", imuData.fusionPose));
+				fflush(stdout);
+				displayTimer = now;
+			}
+
+			if((now - rateTimer) > 1000000)
+			{
+				sampleRate = sampleCount;
+				sampleCount = 0;
+				rateTimer = now;
+			}
+		}
+	}
 
 	return 0;
 }
